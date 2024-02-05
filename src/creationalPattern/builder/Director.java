8e2cff568c17a59f05394c5aa2d172c38ba95745
Ylan <hei.nalisoa@gmail.com>
2024-02-05 19:22:23 +0300

    feat: make car

--- src/creationalPattern/builder/Director.java
+++ src/creationalPattern/builder/Director.java
@@ -1,7 +1,25 @@
 package creationalPattern.builder;
 
 public class Director {
-    Director(Builder builder){
+    private Builder builder;
+
+    public Director(Builder builder) {
+        this.builder = builder;
     }
 
+    public void constructSportsCar() {
+        builder.reset(true);
+        builder.setSeats(2);
+        builder.setEngine("V8");
+        builder.setTripComputer(true);
+        builder.setGPS(true);
+    }
+
+    public void constructSUV() {
+        builder.reset(true);
+        builder.setSeats(5);
+        builder.setEngine("V6");
+        builder.setTripComputer(false);
+        builder.setGPS(true);
+    }
 }
